3b7fe483dbb3553cfc7d3a3482c09a5168dc5ec0
lsm <liusenmiao@gmail.com>
2016-01-31 21:26:12 -0800

    Replace duplexity with net.Socket.

--- benchmark/fd-pub.js
+++ benchmark/fd-pub.js
@@ -1,22 +1,16 @@
 // pub/sub benchmark script credit: https://github.com/tj/axon/tree/master/benchmark
-var fs = require('fs');
-var duplexify = require('duplexify')
+var net = require('net')
 var socketmq = require('../')
 
 var smq = new socketmq.Socket()
-
-/**
- * RPC
- */
-var streamOpts = {
-  fd: 3
-}
-var readable = fs.createReadStream(null, streamOpts);
-var writable = fs.createWriteStream(null, streamOpts);
-var stream = duplexify(writable, readable)
+var stream = net.Socket({
+  fd: 3,
+  readable: true,
+  writable: true
+})
 
 smq.on('connect', function() {
-  var perTick = 1;
+  var perTick = 5;
   var str = Array(1024).join('a');
   console.log('sending %d per tick', perTick);
   console.log('sending %d byte messages', Buffer.byteLength(str));
