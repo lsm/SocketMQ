6b176584df64600488fa8fd1382425d83928aea1
lsm <liusenmiao@gmail.com>
2016-01-31 21:32:35 -0800

    Move everything to `lib/`

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Url = require('url');
-var transport = require('./transport')
-var Socket = exports.Socket = require('./socket')
+var transport = require('./lib/transport')
+var Socket = exports.Socket = require('./lib/socket')
 
 exports.bind = function(url, options) {
   return createSocketMQ('bind', url, options)
@@ -10,17 +10,14 @@ exports.connect = function(url, options) {
   return createSocketMQ('connect', url, options)
 }
 
-function isProtocolSupported(protocol) {
+function createSocketMQ(type, url, options) {
+  var target = Url.parse(url)
+  var protocol = target.protocol.slice(0, -1)
+
   if (!transport[protocol]) {
     var err = 'Transport "' + protocol + '" is not supported. SocketMQ supports: ' + Object.keys(transport).join(', ')
     throw new Error(err)
   }
-  return protocol
-}
-
-function createSocketMQ(type, url, options) {
-  var target = Url.parse(url)
-  var protocol = isProtocolSupported(target.protocol.slice(0, -1))
 
   var socket = new Socket()
   transport[protocol][type](target, socket, options)
