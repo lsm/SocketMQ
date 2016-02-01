7622fa4c89e9e514531ab4ed7d93e9f92fe0f4ce
lsm <liusenmiao@gmail.com>
2016-01-31 21:26:37 -0800

    Minor changes for console info.

--- benchmark/tcp-rep.js
+++ benchmark/tcp-rep.js
@@ -5,11 +5,11 @@ var socketmq = require('../')
 var smq = socketmq.bind('tcp://127.0.0.1:6363')
 
 smq.on('bind', function() {
-  console.log('server bind')
+  console.log('server bound')
 })
 
 smq.on('connect', function(stream) {
-  console.log('new stream connected to server')
+  console.log('new connection')
 })
 
 smq.rep('request.test', function(msg, reply) {
