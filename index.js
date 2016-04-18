bb54b96da826033c76c952c3521b85fb2a3af6d0
lsm <liusenmiao@gmail.com>
2016-04-18 12:27:57 -0700

    Move server side requires into functions.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var Socket = require('./lib/socket/index')
-var SocketHub = require('./lib/socket' + '/hub')
-var SocketGateway = require('./lib/socket' + '/gateway')
 var SocketChannel = require('./lib/socket/channel')
 
 var socketmq = module.exports = function() {
@@ -21,14 +19,21 @@ socketmq.connect = function(uri, options, callback) {
   return smq
 }
 
+socketmq.channel = function(ns, name) {
+  return new SocketChannel(null, ns, name)
+}
+
+/**
+ * Server side API
+ * @return {[type]} [description]
+ */
+
 socketmq.hub = function() {
+  var SocketHub = require('./lib/socket' + '/hub')
   return new SocketHub()
 }
 
 socketmq.gateway = function() {
+  var SocketGateway = require('./lib/socket' + '/gateway')
   return new SocketGateway()
-}
-
-socketmq.channel = function(ns, name) {
-  return new SocketChannel(null, ns, name)
 }
